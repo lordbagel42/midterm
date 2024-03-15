7535c7182021ec93e18f0c8509a2fa889b9087e1
Raygen Rupe <raygenrrupe@gmail.com>
2024-03-14 20:22:16 -0600

    first commit

---
+++ uno.config.ts
@@ -0,0 +1,11 @@
+// uno.config.ts
+import { defineConfig } from 'unocss'
+import presetUno from '@unocss/preset-uno'
+import {presetDaisy} from 'unocss-preset-daisy'
+
+export default defineConfig({
+  presets: [
+    presetUno(),
+	presetDaisy(),
+  ],
+})
