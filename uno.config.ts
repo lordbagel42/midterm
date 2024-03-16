734f5c14669824a73c8e0a177f08af931ee26971
Raygen Rupe <raygenrrupe@gmail.com>
2024-03-16 15:08:22 -0600

    attributify

--- uno.config.ts
+++ uno.config.ts
@@ -2,10 +2,12 @@
 import { defineConfig } from "unocss";
 import presetUno from "@unocss/preset-uno";
 import { presetDaisy } from "unocss-preset-daisy";
+import presetAttributify from '@unocss/preset-attributify';
 
 export default defineConfig({
   presets: [
     presetUno(),
+    presetAttributify(),
     presetDaisy({
       themes: ["light", "dark"],
     }),
