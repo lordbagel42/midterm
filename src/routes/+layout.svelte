891811b02f58d03a0c2256eb1790c7bee446191a
Raygen Rupe <raygenrrupe@gmail.com>
2024-03-27 01:59:10 -0600

    add bagels, fix links

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -13,13 +13,19 @@
 		<!-- App Bar -->
 		<AppBar>
 			<svelte:fragment slot="lead">
-				<strong class="text-xl uppercase">Bagel Industries</strong>
+				<strong class="text-xl uppercase"><a href="/">Bagel Industries</a></strong>
 			</svelte:fragment>
 			<svelte:fragment slot="trail">
-				<a class="btn btn-sm variant-filled-secondary hover:bg-secondary-450 duration-[500ms]" href="/menu" target="_blank" rel="noreferrer">
+				<a
+					class="btn btn-sm variant-filled-secondary hover:bg-secondary-450 duration-[500ms]"
+					href="/menu"
+				>
 					Menu
 				</a>
-				<a class="btn btn-sm variant-filled-primary hover:bg-primary-400 duration-[500ms]" href="/order" target="_blank" rel="noreferrer">
+				<a
+					class="btn btn-sm variant-filled-primary hover:bg-primary-400 duration-[500ms]"
+					href="/order"
+				>
 					Order Now
 				</a>
 			</svelte:fragment>
---
+++ src/routes/menu/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+	// Importing the bagel data JSON file
+	import bagels from './bagelData.json';
+</script>
+
+<div class="flex flex-col items-center justify-center">
+    {#each bagels as bagel}
+        <div class="w-full max-w-screen-lg mx-auto mb-4">
+            <div class="bg-white shadow-md border border-gray-300 rounded-lg p-4 flex"> <!-- Added items-center here -->
+                <div class="mr-4">
+                    <img class="w-96 h-96 object-cover rounded" src={bagel.image} alt={bagel.name} /> <!-- Adjusted width of the image -->
+                </div>
+                <div class="flex flex-col"> <!-- Added flex here -->
+                    <h3 class="font-semibold text-lg">{bagel.name}</h3>
+                    <p class="text-gray-600">{bagel.description}</p>
+                </div>
+            </div>
+        </div>
+    {/each}
+</div>
