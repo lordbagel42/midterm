c80e38f867f19b3d314b9992b609c64ef1bf5930
Raygen Rupe <raygenrrupe@gmail.com>
2024-03-27 18:00:11 -0600

    add basic homepage hero

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import everythingbagel from '$lib/images/everything_bagel-nobg.png'
+</script>
+
+<div class="flex flex-row justify-center items-center bg-surface-200 h-screen">
+    <div class="flex flex-col mr-16"> <!-- Hero Text -->
+        <div class="flex flex-row">
+            <h1 class="text-4xl">Creating the flavors of tomorrow.</h1>
+        </div>
+        <div class="flex flex-row">
+            <p class="text-lg">Discover the flavors that can ignite your senses.</p>
+        </div>
+    </div>
+    <div class="flex flex-col h-screen"> <!-- Hero Image -->
+        <img class="h-3/4 object-cover" src="{everythingbagel}" alt="">
+    </div>
+</div>
