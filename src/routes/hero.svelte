5b44dd51027370bac5814c2de6430f2230363939
Raygen Rupe <raygenrrupe@gmail.com>
2024-03-29 21:37:38 -0600

    separate main route into multiple files

---
+++ src/routes/hero.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import everythingbagel from '$lib/images/everything_bagel-nobg.png';
+</script>
+
+<div class="flex flex-row justify-center items-center bg-surface-200 h-screen">
+	<div class="flex flex-col flex-none mr-16 w-1/3">
+		<!-- Hero Text -->
+		<div class="flex flex-row">
+			<h1 class="text-4xl">Creating the flavors of tomorrow.</h1>
+		</div>
+		<div class="flex flex-row">
+			<p class="text-lg">
+				At Bagel Industries, we believe that every bite of a bagel should be a moment of pure
+				delight. That's why we've dedicated ourselves to creating the most delicious and innovative
+				bagel products.
+			</p>
+		</div>
+	</div>
+	<div class="flex flex-col h-screen">
+		<!-- Hero Text -->
+		<img class="h-3/4 object-cover" src={everythingbagel} alt="" />
+	</div>
+</div>
