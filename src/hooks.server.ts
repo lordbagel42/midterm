8257482ab5817906b98f372f3bb56ec1d6865024
Raygen Rupe <raygenrrupe@gmail.com>
2024-04-23 21:59:54 -0600

    add Sentry

---
+++ src/hooks.server.ts
@@ -0,0 +1,17 @@
+import { sequence } from "@sveltejs/kit/hooks";
+import { handleErrorWithSentry, sentryHandle } from "@sentry/sveltekit";
+import * as Sentry from '@sentry/sveltekit';
+
+Sentry.init({
+  dsn: 'https://7603d836bae2737e52409b3f9fe0dcbb@o4507139426156544.ingest.us.sentry.io/4507139483041792',
+  tracesSampleRate: 1.0,
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: import.meta.env.DEV,  
+});
+
+// If you have custom handlers, make sure to place them after `sentryHandle()` in the `sequence` function.
+export const handle = sequence(sentryHandle());
+
+// If you have a custom error handler, pass it to `handleErrorWithSentry`
+export const handleError = handleErrorWithSentry();
