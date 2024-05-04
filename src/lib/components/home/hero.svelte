d6ff47f5500b49ac7bb01cac3adab6d769d7dc7b
lordbagel <raygenrrupe@gmail.com>
2024-05-04 01:21:22 -0600

    improve

--- src/lib/components/home/hero.svelte
+++ src/lib/components/home/hero.svelte
@@ -1,15 +1,15 @@
 <script lang="ts">
-	import everythingbagel from '$lib/images/everything_bagel-nobg.png';
+	import everythingBagel from '$lib/images/everything_bagel-nobg.png';
 </script>
 
 <div class="flex flex-row justify-center items-center bg-surface h-screen">
-	<div class="flex flex-col flex-none mr-16 w-1/3">
+	<div class="flex flex-col flex-none mr-16 w-1/3 space-y-2">
 		<!-- Hero Text -->
-		<div class="flex flex-row">
-			<h1 class="text-5xl font-bold">Creating the <em>flavors</em> of tomorrow.</h1>
+		<div class="flex flex-row space-x-2">
+			<h1 class="text-5xl font-bold text-left">Creating the <em>flavors</em> of tomorrow.</h1>
 		</div>
 		<div class="flex flex-row">
-			<p class="text-xl">
+			<p class="text-xl text-left">
 				At Bagel Industries, we believe that every bite of a bagel should be a moment of pure
 				delight. That's why we've dedicated ourselves to creating the most delicious and innovative
 				bagel products.
@@ -17,7 +17,14 @@
 		</div>
 	</div>
 	<div class="flex flex-col h-screen">
-		<!-- Hero Text -->
-		<img class="h-3/4 object-cover" src={everythingbagel} alt="" />
+		<!-- Hero Image -->
+		<img
+			class="h-3/4 object-cover"
+			src={everythingBagel}
+			alt="Everything bagel from Bagel Industries"
+			loading="lazy"
+			width="300"
+			height="300"
+		/>
 	</div>
 </div>
