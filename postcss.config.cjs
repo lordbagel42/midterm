1cf4a0cbf2a20b8a2f7d03fc5a092fe35246e5dc
lordbagel <raygenrrupe@gmail.com>
2024-05-04 01:21:19 -0600

    improve

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,6 +1,16 @@
 module.exports = {
-	plugins: {
-		tailwindcss: {},
-		autoprefixer: {}
-	}
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
+
+
+@import 'tailwindcss/base';
+@import 'tailwindcss/components';
+@import 'tailwindcss/utilities';
+
+
+"scripts": {
+  "build:css": "postcss styles/tailwind.css -o styles/output.css"
+}
